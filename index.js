9b23e0f90b320fc2d139c56b56192d482217bafb
Tom Boutell <tom@punkave.com>
2013-11-25 08:09:02 -0500

    Commented out the example code that fakes its way through most of the unit tests

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ module.exports = function(fromFile, callback) {
   // Convert to the other format
   // Generate thumbnail
   // Invoke callback with null and an object containing paths to all three
-  return callback(null, {
-    mp4: 'test.mp4',
-    webm: 'test.webm',
-    jpg: 'test.jpg'
-  });
+
+  // If you were going to cheat the unit tests semi-successfully you might do this:
+  // return callback(null, {
+  //   mp4: 'test.mp4',
+  //   webm: 'test.webm',
+  //   jpg: 'test.jpg'
+  // });
 };
