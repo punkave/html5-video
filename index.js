63595089691c4ae5306535695c220d70af352da6
Tom Boutell <tom@punkave.com>
2013-11-25 08:19:26 -0500

    Warning about accidentally crushing the other test case

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 module.exports = function(fromFile, callback) {
   // Determine format
-  // Convert to the other format
+  // Convert to the other format. Use a file in /tmp or similar, do NOT crush the other test case file (:
   // Generate thumbnail
   // Invoke callback with null and an object containing paths to all three
 
   // If you were going to cheat the unit tests semi-successfully you might do this:
   // return callback(null, {
-  //   mp4: 'test.mp4',
-  //   webm: 'test.webm',
-  //   jpg: 'test.jpg'
+  //   mp4: '/tmp/test.mp4',
+  //   webm: '/tmp/test.webm',
+  //   jpg: '/tmp/test.jpg'
   // });
 };
